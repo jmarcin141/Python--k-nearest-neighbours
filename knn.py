a167a04f26dbea200a8ccefbc10d1b15301a3388
Jakub-PC <jmarcin@student.agh.edu.pl>
2020-02-11 15:39:17 +0100

    first step

---
+++ knn.py
@@ -0,0 +1,17 @@
+# knn - algorithm
+
+input_vector = [0,1,2,3,4]
+
+import numpy as np
+
+
+def knn(input_vector):
+    '''
+        k - nearest neighbours algorithm
+        '''
+
+    return 0
+
+
+x = knn(input_vector)
+print(x)
